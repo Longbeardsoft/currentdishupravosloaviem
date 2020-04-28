6a18e05bbb660f0df047c32f1c2669cd705dca7d
dolgoborodov_ia@nlmk.com <1234567yY>
2020-04-28 21:52:36 +0300

    bible Lopuhin interpretation almost almost finished

--- Bible/ajax_verse.php
+++ Bible/ajax_verse.php
@@ -117,7 +117,86 @@ switch ($id_interpr) {
     case 2:
     {
         switch ($id_book) {
+            case 53:
+                $htm_files[0] = '1Co.htm';
+                $book_id=1;
+                break;
+            case 48:
+                $htm_files[1] = '1Jn.htm';
+                $book_id=2;
+                break;
+            default:
+                $id_book = '-1';
+                $book_id = '-1';
+                break;
 
+/*        $htm_files[2] = '1Ki.htm';
+        $htm_files[3] = '1Ma.htm';
+        $htm_files[4] = '1Par.htm';
+        $htm_files[5] = '1Pe.htm';
+        $htm_files[6] = '1Sa.htm';
+        $htm_files[7] = '1The.htm';
+        $htm_files[8] = '1Ti.htm';
+        $htm_files[9] = '2Co.htm';
+        $htm_files[10] = '2Jn.htm';
+        $htm_files[11] = '2Ki.htm';
+        $htm_files[12] = '2Ma.htm';
+        $htm_files[13] = '2Par.htm';
+        $htm_files[14] = '2Pe.htm';
+        $htm_files[15] = '2Sa.htm';
+        $htm_files[16] = '2The.htm';
+        $htm_files[17] = '2Ti.htm';
+        $htm_files[18] = '3Jn.htm';
+        $htm_files[19] = '3Ma.htm';
+        $htm_files[20] = 'Act.htm';
+        $htm_files[21] = 'Amo.htm';
+        $htm_files[22] = 'Dan.htm';
+        $htm_files[23] = 'Deut.htm';
+        $htm_files[24] = 'Ecc.htm';
+        $htm_files[25] = 'Eccl.htm';
+        $htm_files[26] = 'Eph.htm';
+        $htm_files[27] = 'Est.htm';
+        $htm_files[28] = 'Exod.htm';
+        $htm_files[29] = 'Eze.htm';
+        $htm_files[30] = 'Ezr.htm';
+        $htm_files[31] = 'Ga.htm';
+        $htm_files[32] = 'Gen.htm';
+        $htm_files[14] = 'Hab.htm';
+        $htm_files[33] = 'Heb.htm';
+        $htm_files[34] = 'Hos.htm';
+        $htm_files[35] = 'Is.htm';
+        $htm_files[36] = 'Ja.htm';
+        $htm_files[37] = 'Jd.htm';
+        $htm_files[38] = 'Jdg.htm';
+        $htm_files[39] = 'Jer.htm';
+        $htm_files[40] = 'Jn.htm';
+        $htm_files[41] = 'Job.htm';
+        $htm_files[42] = 'Joel.htm';
+        $htm_files[43] = 'Jonah.htm';
+        $htm_files[44] = 'Josh.htm';
+        $htm_files[45] = 'Lam.htm';
+        $htm_files[46] = 'Lev.htm';
+        $htm_files[47] = 'Lk.htm';
+        $htm_files[48] = 'Mal.htm';
+        $htm_files[49] = 'Mic.htm';
+        $htm_files[50] = 'Mk.htm';
+        $htm_files[51] = 'Mt.htm';
+        $htm_files[52] = 'Nah.htm';
+        $htm_files[53] = 'Neh.htm';
+        $htm_files[54] = 'Num.htm';
+        $htm_files[55] = 'Obad.htm';
+        $htm_files[56] = 'Phm.htm';
+        $htm_files[57] = 'Php.htm';
+        $htm_files[58] = 'Prov.htm';
+        $htm_files[59] = 'Ps.htm';
+        $htm_files[60] = 'Re.htm';
+        $htm_files[61] = 'Ro.htm';
+        $htm_files[62] = 'Ruth.htm';
+        $htm_files[63] = 'Son.htm';
+        $htm_files[64] = 'Tit.htm';
+        $htm_files[65] = 'Zec.htm';
+        $htm_files[66] = 'Zep.htm';
+*/
         }
         $avtorInterpretation = 'Лопухин';
     }
