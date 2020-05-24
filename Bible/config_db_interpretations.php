c1b452a8ce180b2e89bbe0a75ad3355f4cc4ed59
dolgoborodov_ia@nlmk.com <1234567yY>
2020-05-24 23:01:49 +0300

    gitignore rm commit

--- Bible/config_db_interpretations.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'localhost'; // адрес сервера
-$database = "host1382121_bible"; // имя базы данных
-$user = 'host1382121'; // имя пользователя
-$password = '15b47f6a'; // пароль
-//$user = 'root'; // имя пользователя
-//$password = ''; // пароль
