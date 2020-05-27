09a3f2659c4d36b4228377bc4e323d2e670b2aae
dolgoborodov_ia@nlmk.com <1234567yY>
2020-05-27 22:26:09 +0300

    search almost correct

--- Bible/search.php
+++ Bible/search.php
@@ -1,5 +1,9 @@
 <?php
+function colorSearchWord($word,$string,$color='violet'){
+    return preg_replace('/('.preg_quote($word).')/i','<span style="color:'.$color.';border-bottom:1px dashed '.$color.';">$1</span>', mb_strtolower($string));
+}
 $referal = mb_strtolower($_REQUEST['referal']);
+//$referal = $_REQUEST['referal'];
 require_once ('config_db_bible.php');
 define("PREFIX", ""); //Префикс если нужно
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
@@ -8,19 +12,8 @@ if (!empty($referal)) { //Принимаем данные
     //$referal = trim(strip_tags(stripcslashes(htmlspecialchars($referal))));
     $db_referal = $mysqli->query("SELECT distinct text, book_id, book_name, chapter_id, verse_id, translate_id, translate_name from " . PREFIX . "bible WHERE text LIKE '%$referal%' ")
     or die('Ошибка №' . __LINE__ . '<br>Обратитесь к администратору сайта пожалуйста, сообщив номер ошибки.');
-/*    $translate_sinodal=false;
-    $translate_slavic=false;*/
     while ($row = $db_referal->fetch_array()) {
-        /*if (!$translate_sinodal&&$row["translate_id"]==='1') {
-            echo $row["translate_name"];
-            $translate_sinodal=true;
-        }
-        if (!$translate_slavic&&$row["translate_id"]==='2') {
-            echo $row["translate_name"];
-            $translate_slavic=true;
-        }*/
-        $pos = strpos($row["text"], $referal);
-        $text = substr_replace($row["text"], '<span style="color:red;">' . $referal . "</span>", $pos,  strlen($referal));
+        $text=colorSearchWord($referal,$row["text"],'red');
         echo "\n<li id='".$row["book_id"]."-".$row["chapter_id"]."-".$row["verse_id"]."'>".$row["translate_name"]." <span style=\"color:green;\">".$row["book_name"] . ' ' . $row["chapter_id"] . ':' . $row["verse_id"] . '</span> - ' . $text . "</li>"; //$row["name"] - имя поля таблицы
         $i++;
     }
